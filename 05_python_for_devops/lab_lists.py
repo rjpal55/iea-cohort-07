3423c3dc5f32952085e70ec37cc86be20871c2c0
Rich Palermo <richard.palermo@libertymutual.com>
2022-05-06 09:49:42 -0400

    5/6 commit

--- 05_python_for_devops/lab_lists.py
+++ 05_python_for_devops/lab_lists.py
@@ -1,10 +1,12 @@
 from collections import Counter
 list1 = ["a","b","c"]
 list2 = ["a","b","c"]
-user_action = ''
+user_action = ""
+user_pos = ""
+cur_list = ""
 
 while user_action != "quit":
-    user_action = input("Enter action (add, remove, reverse, print, equality, quit): ")
+    user_action = input("Enter action (add, remove, delete (by position), reverse, print, equality, quit): ")
     if user_action == "quit":
         break
     if user_action == "equality":
@@ -13,40 +15,37 @@ while user_action != "quit":
         else:
             print("The lists are not equal")
         continue
+
     user_list = int(input("Enter list (1 or 2): "))
-    if user_action == "print":
-        if user_list == 1:
-            print(list1)
-            continue
-        else:
-            print(list2)
-            continue
-    if user_action == "reverse":
-        if user_list == 1:
-            list1.reverse()
-            print(list1)
-            continue
-        else:
-            list2.reverse()
-            print(list2)
-            continue
-    
-    user_item = input("Enter item: ")
-    
-    if user_action == "add" and user_list == 1:
-        list1.append(user_item)
-    elif user_action == "add" and user_list == 2:
-        list2.append(user_item)
-    elif user_action == "remove" and user_list == 1:
-        if user_item in list1:
-            list1.remove(user_item)
-    elif user_action == "remove" and user_list == 2:
-        if user_item in list2:
-            list2.remove(user_item)
-    elif user_action == "reverse" and user_list == 1:
-        list1.reverse()
+    if user_list == 1:
+        cur_list = list1
     else:
-        list2.reverse()
+        cur_list = list2
+
+    if user_action == "print":
+        for index,item in enumerate(cur_list):
+            print(index,item)
+        continue        
+    if user_action == "reverse":
+        cur_list.reverse()
+        print(cur_list)
+        continue
+    if user_action == "delete":
+        for index,item in enumerate(cur_list):
+            print(index,item)
+        user_pos = int(input("Enter position: "))
+        cur_pos = user_pos
+        cur_list.pop(cur_pos)
+        continue
+    
+    if user_action == "add":
+        print(cur_list)
+        user_item = input("Enter item to add: ")
+        cur_list.append(user_item)
+    elif user_action == "remove":
+        print(cur_list)
+        user_item = input("Enter item to remove: ")
+        cur_list.remove(user_item)
 
 print(list1)
 print(list2)
