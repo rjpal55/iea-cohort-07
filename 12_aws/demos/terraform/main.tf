2f011a6f860ca7994b7090f9120291b4fc192c7e
JR Rickerson <jrrickerson@redrivetstudios.com>
2022-06-30 15:11:45 -0400

    Add CFT and Terraform demos

---
+++ 12_aws/demos/terraform/main.tf
@@ -0,0 +1,37 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "jrrickerson-demo-terraform"
+    course = "LM-IEA"
+    cohort = var.cohort
+  }
+}
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-0d5eff06f840b45e9"
+  instance_type = "t3.medium"
+
+  tags = {
+    Name = "jrrickerson-demo-terraform2"
+    course = "LM-IEA"
+  }
+}
+
---
+++ 12_aws/demos/terraform/variables.tf
@@ -0,0 +1,5 @@
+variable "cohort" {
+  description = "Value of the Cohort tag for the EC2 instance"
+  type        = string
+  default     = "7"
+}
